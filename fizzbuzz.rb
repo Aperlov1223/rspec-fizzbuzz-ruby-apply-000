4a903f15aee9eec9dcd9abc7ebd22a3e9b45b33b
Alice Perlov <aperlov23@gmail.com>
2020-06-30 02:42:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(number)
-  number = gets()
-puts(number)
-if number == % 3
-  puts ('fizz')
-elsif number == % 5
-  puts ('buzz')
-elsif number == %15
-  puts ('fizzbuzz')
+def fizzbuzz(fizz_n)
+  if number % 3 == 0
+   puts "Fizz"
+end
+if number % 5 == 0
+   puts "Buzz"
+end
+  elsif n == % 15
+     "FizzBuzz"
+   end
+  else
+     "nil"
+  end
 end
