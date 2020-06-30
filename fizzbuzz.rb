866d7a84ea606e13ad95d7939f57d9acb62fafb4
Alice Perlov <aperlov23@gmail.com>
2020-06-30 02:21:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if
+    (int_is_divisible_by_3)
+    "Fizz"
+  elsif (int_is_divisible_by_5)
+    "Buzz"
+  elsif (int_is_divisible_by_3_and_5)
+    "FizzBuzz"
+  else
+    puts "nil"
+  end
